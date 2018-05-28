e0c8df1316587ca980bc60febf42932ad9e19105
eudaldromo <eudald.rg@gmail.com>
2018-05-29 00:46:56 +0200

    finish formula sheet

--- formula_sheet.R
+++ formula_sheet.R
@@ -5,7 +5,12 @@ require(ape)
 require(cclust) # ?clusplot (better explained at ?clusplot.default)
 require(cluster)
 require(clusterSim) # lda and qda or predict or something
-require(MVN) # Multivariate normality test
+require(mvnTest) # Multivariate normality test
+require(factoextra)
+require(MASS)
+require(biotools) # Covariance Homogeneity test
+require(ICSNP) # T2
+require(Hotelling) # T2 Permutation Test
 source("./Utils.R")
 
 data <- read.table("./2nda_parte/ecosystems_windows.txt",sep="\t",header=T)
@@ -38,15 +43,25 @@ data.dist <- vegdist(data.numerical, method="mahalanobis")
 
 ## Method
 # Hierarchical (UPGMA/Ward2)
-
+data.hierarchical <- hclust(data.dist, method="average") # or single, complete, or ward.D2
+plot(data.hierarchical, hang=-1) #, labels=data[,1])
+data.cophenetic = cophenetic(clusterAV)
+plot(data.cophenetic, data.dist)
 
 # Kmeans/Pam (it's still kmeans, but R calls it this way)
 number_of_clusters <- 2
 data.kmeans <- kmeans(data.scaled, centers = number_of_clusters)
+data$cluster <- data.kmeans$cluster
 
+# Pam
 # Can also be called with diss = F and a matrix of observations, but then we need to specify metric
 # only supports euclidean and manhattan
+# Only needs silhouettes. They are already computed
 data.pam <- pam(countries.dist, number_of_clusters, diss = TRUE)
+# set the clustering
+
+# Fuzzy (non hard boundaries, everything is diffuminated)
+data.fuzzy <- fanny(countries.MAH, 2, diss = TRUE, memb.exp=1.2)
 
 ## Stop criterion (for kmeans)
 # Silhouettes
@@ -86,33 +101,63 @@ colnames(results)<-c("K","TOTSS","WITHINSS","PseudoF","AvgSilh","DeltaTESS")
 ### Representatives description
 
 ### 2D Representation
-# PCA
-pca <- princomp(data.scaled)
-# MDS
 
-# Both can use clusplot with diss = T or F
+# Both PCA and MDS can use clusplot with diss = T or F
 clusplot(data.scaled, data.kmeans$cluster, 
          col.txt = c("blue", "red")[data.kmeans$cluster], 
          labels = 2, color = TRUE, main = "Kmeans plot", cex = 0)
+
+# If you need a fancier thing, compute it yourself:
+# PCA
+pca <- princomp(data.scaled)
+# plot() weights of the n components you want. add arrows of the loadings you need
+
+# MDS
+mds = cmdscale(data.D2, 3, eig=TRUE)
+plot(mds$points[,1], mds$points[,2],asp=1, xlab="Axis 1", ylab="Axis 2")
+names = rownames(data)
+text(mds$points[,1], mds$points[,2], labels= names, pos=2, cex=0.5, offset=0.15)
 
 # FA
 # CA
 
 ### Group differentiation
+g1<-as.matrix(data[which(data$cluster==1),-8]) # Take cluster out
+g2<-as.matrix(data[which(data$cluster==2),-8]) # Take cluster out
 
-## Multivariate Normal case
+## Multivariate Normal case H0 not normal mv
+mvn(g1, mvnTest = "mardia") # can also do royston and henze-zirkler
+AD.test(g1, qqplot = T) # hztest or roystontest for others
+
 # Covariance matrix homogeneity
+boxM(data[,-8], data[,8])
+# or manova
+manova<-manova(cbind(param_1, param_2, param_3)~cluster, data = data) #relate everything to clust
+summary(efinsa.manova, test = "Pillai")
+
 # Means homogeneity
+HotellingsT2(g1, g2, test = "chi")
 
 ## Non-normal
 # Permutation test
+Hotelling.perm12<-hotelling.test(g1, g2, perm = TRUE, progBar=F)
+Hotelling.perm12$pval
 
 ### Discriminant analysis
 
 ## Prediction
-# LDA
-# QDA
-# Logistic
+
+# LDA/QDA (just change the function)
+data.fit <- lda(cluster ~ ., data = data, na.action = "na.omit", CV = T) # Linear and crossvalidated
+data.model <- lda(cluster ~ ., data = data, na.action = "na.omit", CV = F)
+
+# Logistic (only found code for 2 groups)
+data.model <- glm(cluster ~ ., family="binomial", data=dfx)
+
+# Actual prediction
+element <- data.frame(param_a = 251, param_b = 241)
+predict(element, data.model)
 
 ## Quality
 ## Cross-validation
+sum(diag(prop.table(table(data$cluster, data.fit$class)))) # how good it is
